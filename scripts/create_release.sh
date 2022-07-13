2a9bb5d89f362773f680ba0ef54842aa575a6be7
Dhuan Oliveira <dhuanco@gmail.com>
2022-07-13 15:42:36 +0200

    Renaming vars

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,16 +1,16 @@
 set -ex
 
-MOCK_VERSION=$(echo $GITHUB_REF | cut -d '/' -f 3)
+APP_VERSION=$(echo $GITHUB_REF | cut -d '/' -f 3)
 
 export GH_TOKEN=${GH_KEY}
 
-echo $MOCK_VERSION
+echo $APP_VERSION
 
-gh release create "$MOCK_VERSION" -t "$MOCK_VERSION"
+gh release create "$APP_VERSION" -t "$APP_VERSION"
 
 RELEASE_FILES=$(ls ./release_downloads/*.zip)
 
 for RELEASE_FILE in $RELEASE_FILES
 do
-    gh release upload "$MOCK_VERSION" "$RELEASE_FILE"
+    gh release upload "$APP_VERSION" "$RELEASE_FILE"
 done
