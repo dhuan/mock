59b4d362fdd96f2079dca66324cb6cfd63e6b8d1
Dhuan Oliveira <dhuanco@gmail.com>
2022-08-07 19:53:30 +0200

    Move assert validating functions to their own file + Remove unused type

---
+++ internal/mock/asserters.go
@@ -0,0 +1,177 @@
+package mock
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/dhuan/mock/internal/types"
+	. "github.com/dhuan/mock/pkg/mock"
+)
+
+func assertHeaderMatch(requestRecord *types.RequestRecord, assert *AssertOptions) (*[]ValidationError, error) {
+	validationErrors := make([]ValidationError, 0)
+	keyValues := assert.KeyValues
+	if keyValues == nil {
+		keyValues = make(map[string]interface{})
+	}
+
+	if assert.Key != "" && assert.Value != "" {
+		keyValues[assert.Key] = fmt.Sprint(assert.Value)
+	}
+
+	for i, _ := range keyValues {
+		key := i
+		value := keyValues[i]
+
+		valueFromRequestRecord, ok := requestRecord.Headers[key]
+		if !ok {
+			validationErrors = append(validationErrors, ValidationError{
+				Code: ValidationErrorCode_HeaderNotIncluded,
+				Metadata: map[string]string{
+					"missing_header_key": key,
+				},
+			})
+
+			continue
+		}
+
+		if value != strings.Join(valueFromRequestRecord, "") {
+			validationErrors = append(validationErrors, ValidationError{
+				Code: ValidationErrorCode_HeaderValueMismatch,
+				Metadata: map[string]string{
+					"header_key":             key,
+					"header_value_requested": strings.Join(valueFromRequestRecord, ""),
+					"header_value_expected":  value.(string),
+				},
+			})
+		}
+	}
+
+	return &validationErrors, nil
+}
+
+func assertMethodMatch(requestRecord *types.RequestRecord, assert *AssertOptions) (*[]ValidationError, error) {
+	validationErrors := make([]ValidationError, 0)
+
+	if requestRecord.Method != assert.Value {
+		validationErrors = append(validationErrors, ValidationError{
+			Code: ValidationErrorCode_MethodMismatch,
+			Metadata: map[string]string{
+				"method_requested": requestRecord.Method,
+				"method_expected":  assert.Value,
+			},
+		})
+	}
+
+	return &validationErrors, nil
+}
+
+func assertFormMatch(requestRecord *types.RequestRecord, assert *AssertOptions) (*[]ValidationError, error) {
+	validationErrors := make([]ValidationError, 0)
+	requestBody := string(*requestRecord.Body)
+
+	parsedForm, err := parseForm(requestBody)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, _ := range assert.KeyValues {
+		value, ok := parsedForm[i]
+		if !ok {
+			validationErrors = append(
+				validationErrors,
+				ValidationError{
+					Code: ValidationErrorCode_FormKeyDoesNotExist,
+					Metadata: map[string]string{
+						"form_key": i,
+					},
+				},
+			)
+
+			continue
+		}
+
+		if value != assert.KeyValues[i] {
+			validationErrors = append(
+				validationErrors,
+				ValidationError{
+					Code: ValidationErrorCode_FormValueMismatch,
+					Metadata: map[string]string{
+						"form_key":             i,
+						"form_value_requested": value,
+						"form_value_expected":  assert.KeyValues[i].(string),
+					},
+				},
+			)
+		}
+	}
+
+	return &validationErrors, nil
+}
+
+func assertJsonBodyMatch(jsonValidate JsonValidate) func(requestRecord *types.RequestRecord, assert *AssertOptions) (*[]ValidationError, error) {
+	return func(requestRecord *types.RequestRecord, assert *AssertOptions) (*[]ValidationError, error) {
+		validationErrors := make([]ValidationError, 0)
+
+		var jsonResult map[string]interface{}
+		err := json.Unmarshal(*requestRecord.Body, &jsonResult)
+		if err != nil {
+			return &validationErrors, err
+		}
+
+		jsonValidationResult := jsonValidate(jsonResult, assert.Data)
+		if !jsonValidationResult {
+			assertJson, err := json.Marshal(assert.Data)
+			if err != nil {
+				panic(err)
+			}
+
+			requestRecordReformatted, err := reformatJson(requestRecord.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			validationErrors = append(
+				validationErrors,
+				ValidationError{Code: ValidationErrorCode_BodyMismatch, Metadata: map[string]string{
+					"body_requested": string(requestRecordReformatted),
+					"body_expected":  string(assertJson),
+				}},
+			)
+		}
+
+		return &validationErrors, nil
+	}
+}
+
+func reformatJson(jsonEncoded *[]byte) ([]byte, error) {
+	var result map[string]interface{}
+	err := json.Unmarshal(*jsonEncoded, &result)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	newJsonEncoded, err := json.Marshal(result)
+	if err != nil {
+		return []byte(""), err
+	}
+
+	return newJsonEncoded, nil
+}
+
+func parseForm(requestBody string) (map[string]string, error) {
+	formValues := make(map[string]string)
+
+	values, err := url.ParseQuery(requestBody)
+	if err != nil {
+		return formValues, err
+	}
+
+	for i, _ := range values {
+		formValues[i] = values[i][0]
+	}
+
+	return formValues, nil
+}
