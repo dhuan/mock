1d4fe3ce177b0b442402892bb60c8a7b56063f6d
Dhuan Oliveira <dhuanco@gmail.com>
2023-05-07 14:44:10 +0200

    progress

--- internal/mock/response_condition_nth_test.go
+++ internal/mock/response_condition_nth_test.go
@@ -12,44 +12,109 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_ResolveEndpointResponse_Condition_Nth_1(t *testing.T) {
-	requestMock = httptest.NewRequest(
-		http.MethodGet,
-		"/",
-		strings.NewReader(""),
-	)
-	osMockInstance := osMock{}
-	execMockInstance := execMock{}
-	endpointConfig := types.EndpointConfig{
-		Route:    "foo/bar",
-		Method:   "get",
-		Response: []byte(`default response.`),
-		ResponseIf: []types.ResponseIf{
-			{
-				Response: []byte(`this is the second response.`),
-				Condition: &Condition{
-					Type:  ConditionType_Nth,
-					Value: "2",
-				},
+func newRequestRecord(route, method string) types.RequestRecord {
+	return types.RequestRecord{
+		Route:  route,
+		Method: method,
+	}
+}
+
+var endpoint_config_with_nth_conditions = types.EndpointConfig{
+	Route:    "foo/bar",
+	Method:   "get",
+	Response: []byte(`default response.`),
+	ResponseIf: []types.ResponseIf{
+		{
+			Response: []byte(`this is the second response.`),
+			Condition: &Condition{
+				Type:  ConditionType_Nth,
+				Value: "2",
 			},
 		},
-	}
+		{
+			Response: []byte(`this is the third response.`),
+			Condition: &Condition{
+				Type:  ConditionType_Nth,
+				Value: "3",
+			},
+		},
+	},
+}
+
+func Test_ResolveEndpointResponse_Condition_Nth_FirstRequest(t *testing.T) {
+	osMockInstance := osMock{}
+	execMockInstance := execMock{}
 
 	response, _, _ := mock.ResolveEndpointResponse(
 		osMockInstance.ReadFile,
 		execMockInstance.Exec,
-		requestMock,
+		newRequest("foo/bar", http.MethodGet),
 		requestBody,
 		&state,
-		&endpointConfig,
+		&endpoint_config_with_nth_conditions,
 		map[string]string{},
 		map[string]string{},
-		[]types.RequestRecord{},
+		[]types.RequestRecord{
+			newRequestRecord("irrelevant_request", "get"),
+			newRequestRecord("irrelevant_request", "post"),
+		},
 	)
 
-	assert.Equal(
-		t,
-		[]byte(`default response.`),
-		response.Body,
+	assert.Equal(t, []byte(`default response.`), response.Body)
+}
+
+func Test_ResolveEndpointResponse_Condition_Nth_SecondRequest(t *testing.T) {
+	osMockInstance := osMock{}
+	execMockInstance := execMock{}
+
+	response, _, _ := mock.ResolveEndpointResponse(
+		osMockInstance.ReadFile,
+		execMockInstance.Exec,
+		newRequest("foo/bar", http.MethodGet),
+		requestBody,
+		&state,
+		&endpoint_config_with_nth_conditions,
+		map[string]string{},
+		map[string]string{},
+		[]types.RequestRecord{
+			newRequestRecord("irrelevant_request", "get"),
+			newRequestRecord("foo/bar", "get"),
+			newRequestRecord("irrelevant_request", "get"),
+			newRequestRecord("foo/bar", "post"),
+			newRequestRecord("foo/bar", "put"),
+		},
 	)
+
+	assert.Equal(t, []byte(`this is the second response.`), response.Body)
+}
+
+func Test_ResolveEndpointResponse_Condition_Nth_ThirdRequest(t *testing.T) {
+	osMockInstance := osMock{}
+	execMockInstance := execMock{}
+
+	response, _, _ := mock.ResolveEndpointResponse(
+		osMockInstance.ReadFile,
+		execMockInstance.Exec,
+		newRequest("foo/bar", http.MethodGet),
+		requestBody,
+		&state,
+		&endpoint_config_with_nth_conditions,
+		map[string]string{},
+		map[string]string{},
+		[]types.RequestRecord{
+			newRequestRecord("irrelevant_request", "get"),
+			newRequestRecord("foo/bar", "get"),
+			newRequestRecord("irrelevant_request", "get"),
+			newRequestRecord("foo/bar", "get"),
+		},
+	)
+
+	assert.Equal(t, []byte(`this is the third response.`), response.Body)
+}
+
+func newRequest(route, method string) *http.Request {
+	requestMock = httptest.NewRequest(http.MethodGet, "/", strings.NewReader(""))
+	requestMock.RequestURI = "foo/bar"
+
+	return requestMock
 }
