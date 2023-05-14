0622d08577513dfb5c450d722a55920ad030b470
Dhuan Oliveira <dhuanco@gmail.com>
2023-05-14 22:38:29 +0200

    Add e2e tests covering header matching for conditional responses

---
+++ tests/e2e/conditional_responses_header_match_test.go
@@ -0,0 +1,48 @@
+package tests_e2e
+
+import (
+	"testing"
+
+	. "github.com/dhuan/mock/tests/e2e/utils"
+)
+
+func Test_E2E_ConditionalResponses_HeaderMatch_DefaultResponse(t *testing.T) {
+	headersThatWillNotMatch := []map[string]string{
+		nil,
+		{"some_header": "some_value"},
+		{"some_header": "some_value", "another_header": "another_value"},
+		{"foo": "baar"},
+		{"fooo": "bar"},
+	}
+
+	for i := range headersThatWillNotMatch {
+		RunTest(
+			t,
+			"config_with_conditional_response/config.json",
+			"GET",
+			"conditional_response/header_match",
+			headersThatWillNotMatch[i],
+			"",
+			StringMatches("Default response"),
+		)
+	}
+}
+
+func Test_E2E_ConditionalResponses_HeaderMatch_MatchConditionalResponse(t *testing.T) {
+	headersThatWillMatch := []map[string]string{
+		{"foo": "bar"},
+		{"hello": "world", "foo": "bar"},
+	}
+
+	for i := range headersThatWillMatch {
+		RunTest(
+			t,
+			"config_with_conditional_response/config.json",
+			"GET",
+			"conditional_response/header_match",
+			headersThatWillMatch[i],
+			"",
+			StringMatches("Conditional response with Header Match resolved."),
+		)
+	}
+}
---
+++ tests/e2e/conditional_responses_method_match_test.go
@@ -0,0 +1,34 @@
+package tests_e2e
+
+import (
+	"testing"
+
+	. "github.com/dhuan/mock/tests/e2e/utils"
+)
+
+func Test_E2E_ConditionalResponses_MethodMatch_DefaultResponse(t *testing.T) {
+	RunTest(
+		t,
+		"config_with_conditional_response/config.json",
+		"GET",
+		"conditional_response/method_match",
+		nil,
+		"",
+		StringMatches("Default response"),
+	)
+}
+
+// Unfortunately the following the test is not possible yet.
+/*
+func Test_E2E_ConditionalResponses_MethodMatch_ConditionalResponseMatch(t *testing.T) {
+	RunTest(
+		t,
+		"config_with_conditional_response/config.json",
+		"POST",
+		"conditional_response/method_match",
+        nil,
+		"",
+		StringMatches("Conditional response with Method Match resolved."),
+	)
+}
+*/
