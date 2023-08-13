ca437a3ba3fe5ccca525b1c0d9725ab40399e903
Dhuan Oliveira <dhuanco@gmail.com>
2023-08-13 09:49:34 +0200

    Add test case for base api

--- tests/e2e/base_api_test.go
+++ tests/e2e/base_api_test.go
@@ -8,11 +8,48 @@ import (
 	. "github.com/dhuan/mock/tests/e2e/utils"
 )
 
+func Test_E2E_BaseApi_NormalRequest(t *testing.T) {
+	state := NewState()
+	killMockBase, _, _ := RunMockBg(
+		state,
+		strings.Join([]string{
+			"serve",
+			"-p {{TEST_E2E_PORT}}",
+			"--route 'foo/bar'",
+			"--status-code 205",
+			"--response 'Hello world! This is the base API.'",
+		}, " "),
+		nil,
+	)
+	defer killMockBase()
+
+	RunTestWithNoConfigAndWithArgs(
+		t,
+		[]string{
+			fmt.Sprintf("--base 'localhost:%d'", state.Port),
+			"--route hello/world",
+			"--response 'Hello world!'",
+		},
+		"GET",
+		"hello/world",
+		nil,
+		strings.NewReader(""),
+		StatusCodeMatches(200),
+		StringMatches("Hello world!"),
+	)
+}
+
 func Test_E2E_BaseApi_RequestForwardedToBaseApi(t *testing.T) {
 	state := NewState()
 	killMockBase, _, _ := RunMockBg(
 		state,
-		"serve -p {{TEST_E2E_PORT}} --route 'foo/bar' --status-code 205 --response 'Hello world! This is the base API.'",
+		strings.Join([]string{
+			"serve",
+			"-p {{TEST_E2E_PORT}}",
+			"--route 'foo/bar'",
+			"--status-code 205",
+			"--response 'Hello world! This is the base API.'",
+		}, " "),
 		nil,
 	)
 	defer killMockBase()
