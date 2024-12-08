fa4ab3de6096a00339a1c8318f4950da8ac1898a
Dhuan Oliveira <dhuanco@gmail.com>
2024-12-08 22:43:35 +0100

    Add support for getting request payload's JSON fields

--- internal/cmd/get_payload.go
+++ internal/cmd/get_payload.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -19,7 +22,39 @@ var getPayloadCmd = &cobra.Command{
 				panic(err)
 			}
 
+			if len(args) > 0 {
+				fieldName := args[0]
+
+				if isJsonRequest(request) {
+					var data map[string]interface{}
+					err = json.Unmarshal(fileContent, &data)
+
+					value, ok := data[fieldName]
+					if !ok {
+						os.Exit(1)
+
+						return
+					}
+
+					fmt.Printf("%s\n", value)
+				}
+
+				return
+			}
+
 			fmt.Printf(string(fileContent))
 		})
 	},
+}
+
+func isJsonRequest(request *http.Request) bool {
+	for headerKey := range request.Header {
+		headerValue := strings.Join(request.Header[headerKey], "")
+
+		if strings.ToLower(headerKey) == "content-type" && headerValue == "application/json" {
+			return true
+		}
+	}
+
+	return false
 }
--- tests/e2e/get_payload_test.go
+++ tests/e2e/get_payload_test.go
@@ -24,3 +24,43 @@ func Test_E2E_GetPayload_AllPayload(t *testing.T) {
 		StringMatches("Hello, world. This is the payload."),
 	)
 }
+
+func Test_E2E_GetPayload_GetJsonField_OK(t *testing.T) {
+	RunTestWithNoConfigAndWithArgs(
+		t,
+		[]string{
+			"--route foo/bar",
+			fmt.Sprintf("--exec '%s'", strings.Join([]string{
+				`{{MOCK_EXECUTABLE}} get-payload foo | {{MOCK_EXECUTABLE}} write`,
+			}, ";")),
+		},
+		"GET",
+		"foo/bar",
+		map[string]string{
+			"content-type": "application/json",
+		},
+		strings.NewReader(`{"foo": "bar"}`),
+		StringMatches("bar\n"),
+	)
+}
+
+func Test_E2E_GetPayload_GetJsonField_FieldDoesNotExist(t *testing.T) {
+	RunTestWithNoConfigAndWithArgs(
+		t,
+		[]string{
+			"--route foo/bar",
+			fmt.Sprintf("--exec '%s'", strings.Join([]string{
+				`{{MOCK_EXECUTABLE}} get-payload foo | {{MOCK_EXECUTABLE}} write`,
+				`{{MOCK_EXECUTABLE}} get-payload foo`,
+				`printf $? | {{MOCK_EXECUTABLE}} write -a`,
+			}, ";")),
+		},
+		"GET",
+		"foo/bar",
+		map[string]string{
+			"content-type": "application/json",
+		},
+		strings.NewReader(`{"hello": "world"}`),
+		StringMatches("1"),
+	)
+}
