e68326264abe33f2199ee73a52a6c72c1d11f08e
Dhuan Oliveira <dhuanco@gmail.com>
2025-01-18 23:52:44 +0100

    More test improvements

--- tests/e2e/get_route_param_test.go
+++ tests/e2e/get_route_param_test.go
@@ -1,44 +1,33 @@
 package tests_e2e
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	. "github.com/dhuan/mock/tests/e2e/utils"
 )
 
 func Test_E2E_GetRouteParam(t *testing.T) {
-	RunTestWithNoConfigAndWithArgs(
-		t,
+	RunTest4(
+		t, nil,
 		[]string{
 			"--route say_hi/{name}/{location}",
-			fmt.Sprintf("--exec '%s'", strings.Join([]string{
-				`printf "Hi. My name is $({{MOCK_EXECUTABLE}} get-route-param name). I live on $({{MOCK_EXECUTABLE}} get-route-param location)." | {{MOCK_EXECUTABLE}} write`,
-			}, ";")),
+			CmdExec(`printf "Hi. My name is $({{MOCK_EXECUTABLE}} get-route-param name). I live on $({{MOCK_EXECUTABLE}} get-route-param location)." > $MOCK_RESPONSE_BODY`),
 		},
-		"GET",
-		"say_hi/john_doe/earth",
-		nil,
-		nil,
+		Get("say_hi/john_doe/earth", nil),
 		StringMatches("Hi. My name is john_doe. I live on earth."),
+		ExitCodeHeaderMatches("0"),
 	)
 }
 
 func Test_E2E_GetRouteParam_WithUnexistingKeyExitsWith1(t *testing.T) {
-	RunTestWithNoConfigAndWithArgs(
-		t,
+	RunTest4(
+		t, nil,
 		[]string{
 			"--route say_hi/{name}",
-			fmt.Sprintf("--exec '%s'", strings.Join([]string{
-				`{{MOCK_EXECUTABLE}} get-route-param this-param-does-not-exist`,
-				`printf "Exit code is %d". "${?}" | {{MOCK_EXECUTABLE}} write`,
-			}, ";")),
+			CmdExec(`{{MOCK_EXECUTABLE}} get-route-param this-param-does-not-exist > $MOCK_RESPONSE_BODY`),
 		},
-		"GET",
-		"say_hi/john_doe",
-		nil,
-		nil,
-		StringMatches("Exit code is 1."),
+		Get("say_hi/john_doe", nil),
+		StringMatches(""),
+		ExitCodeHeaderMatches("1"),
 	)
 }
